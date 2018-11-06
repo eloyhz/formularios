3ca40a498145bafa40d1e7564f1cf1cb011213a3
eloyhz <eloyhz@gmail.com>
2018-11-06 09:00:20 -0600

    Catalogo

--- src/app/form-productos/form-productos.component.ts
+++ src/app/form-productos/form-productos.component.ts
@@ -7,21 +7,57 @@ import { Producto } from '../modelos/producto';
   styleUrls: ['./form-productos.component.css']
 })
 export class FormProductosComponent implements OnInit {
-
+  catalogo: Producto[];
+  ultimoId: number;
   producto: Producto;
 
   constructor() {
+    this.catalogo = [];
+    this.ultimoId = 0;
     this.producto = new Producto();
-    this.producto.nombre = '';
-    this.producto.esNacional = true;
-    this.producto.pais = 'mx';
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   enviar(formulario) {
     console.log(formulario);
+    this.producto.id = ++this.ultimoId;
+    this.catalogo.push(this.producto);
+    this.producto = new Producto();
+    console.log(this.catalogo);
   }
 
+  eliminar(id: number) {
+    this.catalogo = this.catalogo.filter(function(p) {
+      return p.id !== id;
+    });
+  }
+
+  editar(id: number) {
+    Object.assign(
+      this.producto,
+      this.catalogo
+        .filter(function(p) {
+          return p.id === id;
+        })
+        .pop()
+    );
+  }
+
+  actualizar() {
+    const id = this.producto.id;
+    Object.assign(
+      this.catalogo
+        .filter(function(p) {
+          return p.id === id;
+        })
+        .pop(),
+      this.producto
+    );
+    this.producto = new Producto();
+  }
+
+  cancelar() {
+    this.producto = new Producto();
+  }
 }
--- src/app/modelos/producto.ts
+++ src/app/modelos/producto.ts
@@ -1,6 +1,13 @@
 export class Producto {
+  id: number;
   nombre: string;
   precio: number;
   esNacional: boolean;
   pais: string;
+
+  constructor() {
+    this.nombre = '';
+    this.esNacional = true;
+    this.pais = 'mx';
+  }
 }
